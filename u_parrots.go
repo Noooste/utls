9839c586964717d522bdfdc175e0ba2ddacc8678
Noooste <you@example.com>
2023-12-14 14:42:23 +0100

    [~] replace import

--- u_parrots.go
+++ u_parrots.go
@@ -15,7 +15,7 @@ import (
 	"sort"
 	"strconv"
 
-	"github.com/refraction-networking/utls/dicttls"
+	"github.com/Noooste/utls/dicttls"
 )
 
 var ErrUnknownClientHelloID = errors.New("tls: unknown ClientHelloID")
